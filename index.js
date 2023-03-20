6da5597d42dcafc11b6e005a9f8d5d8937fa2e8a
jacob <jacobyjones36@gmail.com>
2023-03-20 13:45:19 -0600

    hey

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-function getPlayerName() {
-    return localStorage.getItem('userName') ?? 'Unknown User';
+
+class user{
+    constructor(){
+        const nameOfUser = document.querySelector('.user-name');
+        nameOfUser.textContent = this.getPlayerName();
+    }
+    getPlayerName() {
+        return localStorage.getItem('userName') ?? 'Unknown User';
+    }
 }
